10807defc8bae5677fc31b0d560ca2bd1d043317
Daniel Tschinder <duck+github@mailbox.org>
2020-12-22 18:54:03 +0000

    chore: Refactor flacky test

--- test/error.spec.js
+++ test/error.spec.js
@@ -6,18 +6,11 @@ const examplePath = (fileName) => path.join(__dirname, 'example', fileName);
 const pattern = examplePath('*.txt');
 
 describe('fails', () => {
-  let fileProcessor = null;
-  beforeEach(() => {
-    fileProcessor = new FileProcessor(pattern, workerPath);
-  });
-
-  test('reports error in worker', () => {
-    expect.assertions(1);
-    return new Promise((resolve) => {
-      fileProcessor.on('error', (err) => {
-        expect(err).toEqual(new Error('Not today'));
-        resolve();
-      });
+  test('reports error in worker', (done) => {
+    const fileProcessor = new FileProcessor(pattern, workerPath);
+    fileProcessor.on('error', (err) => {
+      expect(err).toEqual(new Error('Not today'));
+      done();
     });
   });
 });
